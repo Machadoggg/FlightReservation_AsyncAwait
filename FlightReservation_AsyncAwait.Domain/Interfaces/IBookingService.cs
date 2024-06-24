dbae70700954f21fdfb53fcaa350bc1f047f1dff
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 01:19:36 -0500

    Add IBookingService interface with BookFlightAsync method in domain

---
+++ FlightReservation_AsyncAwait.Domain/Interfaces/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using FlightReservation_AsyncAwait.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace FlightReservation_AsyncAwait.Domain.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<string> BookFlightAsync(Flight flight);
+    }
+}
