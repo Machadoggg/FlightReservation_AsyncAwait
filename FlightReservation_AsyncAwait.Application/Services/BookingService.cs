607f42a6bbf01c406991dc59ed3e9f30a6d1c0f0
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 01:34:01 -0500

    Add BookingService class implement IBookingService

---
+++ FlightReservation_AsyncAwait.Application/Services/BookingService.cs
@@ -0,0 +1,16 @@
+﻿using FlightReservation_AsyncAwait.Domain.Entities;
+using FlightReservation_AsyncAwait.Domain.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace FlightReservation_AsyncAwait.Application.Services
+{
+    public class BookingService : IBookingService
+    {
+        public async Task<string> BookFlightAsync(Flight flight)
+        {
+            await Task.Delay(2000); // Simulate async operation
+            return $"BK{new Random().Next(1000, 9999)}";
+        }
+    }
+}
