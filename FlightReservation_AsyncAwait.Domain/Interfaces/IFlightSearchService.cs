29a5e96133a6298418dbef4e62ebd29747a3d11f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 01:16:21 -0500

    Add IFlightSearchService interface in Domain

---
+++ FlightReservation_AsyncAwait.Domain/Interfaces/IFlightSearchService.cs
@@ -0,0 +1,14 @@
+﻿using FlightReservation_AsyncAwait.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FlightReservation_AsyncAwait.Domain.Interfaces
+{
+    public interface IFlightSearchService
+    {
+        public interface IFlightSearchService
+        {
+            Task<List<Flight>> SearchFlightsAsync(string origin, string destination);
+        }
+    }
+}
