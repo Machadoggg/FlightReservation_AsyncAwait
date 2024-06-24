095b29f8aece135eb4c796f3a8318a8877476d04
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 01:35:42 -0500

    Add PaymentService class implement IPaymentService

---
+++ FlightReservation_AsyncAwait.Application/Services/PaymentService.cs
@@ -0,0 +1,14 @@
+﻿using FlightReservation_AsyncAwait.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace FlightReservation_AsyncAwait.Application.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public async Task<bool> ProcessPaymentAsync(decimal amount)
+        {
+            await Task.Delay(3000); // Simulate async operation
+            return true;
+        }
+    }
+}
