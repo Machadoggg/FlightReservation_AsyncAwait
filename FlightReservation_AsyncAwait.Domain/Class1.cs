d79ee688a7965445396497428d451a773a63ad28
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-24 01:13:55 -0500

    Add Flight entitiy in Domain

--- FlightReservation_AsyncAwait.Domain/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace FlightReservation_AsyncAwait.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ FlightReservation_AsyncAwait.Domain/Entities/Flight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FlightReservation_AsyncAwait.Domain.Entities
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; } = default!;
+        public string Origin { get; set; } = default!;
+        public string Destination { get; set; } = default!;
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public decimal Price { get; set; }
+    }
+}
